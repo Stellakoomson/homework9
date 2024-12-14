ec06ce4033a2717a0992a1c4d187ea5f7b713284
Stellakoomson <stellakoomson006@gmail.com>
2024-12-14 16:18:53 +0000

    adding all files

---
+++ src/index.js
@@ -0,0 +1,15 @@
+function changepcolour(){
+
+
+p.style.backgroundColor = "pink";
+p.style.color = "orange";
+
+
+let p = document.querySelector("p");
+p.addEventListener("click", changepcolour);
+
+
+
+
+
+}
